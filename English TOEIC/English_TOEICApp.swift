c5988e3b3b05c517a1845488d53e37ff1a23a044
Paul <paul.colombier2@hotmail.fr>
2020-09-17 17:35:58 +0200

    Initial Commit

---
+++ English TOEIC/English_TOEICApp.swift
@@ -0,0 +1,17 @@
+//
+//  English_TOEICApp.swift
+//  English TOEIC
+//
+//  Created by Paul Colombier on 17/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct English_TOEICApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
